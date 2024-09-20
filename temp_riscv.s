d0f7dc5d01f73fc681f2ef7c472180b615b4e8d6
ShuruiFang (cccxi from NKU) <145469185+cxxxxxxxi@users.noreply.github.com>
2024-09-20 16:49:31 +0800

    Update temp_riscv.s

--- temp_riscv.s
+++ temp_riscv.s
@@ -1,8 +1,3 @@
-	.file	"temp.c"
-	.option nopic
-	.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
-	.attribute unaligned_access, 0
-	.attribute stack_align, 16
 	.text
 	.align	1
 	.globl	multi
